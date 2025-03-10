3e4b99d62f190210b62acd61e2eb87d6664c6545
mohBgz <bouguezine.mohamed@gmail.com>
2025-03-10 18:53:21 +0100

    responsive nav-bar

---
+++ script.js
@@ -0,0 +1,33 @@
+hamburger_icon = document.querySelector(".hamburger-icon");
+
+if (hamburger_icon) {
+    hamburger_icon.addEventListener("click", ()=> {
+        document.querySelector(".hamburger-icon").classList.toggle("active");
+        document.querySelector(".hamburger-menu").classList.toggle("active");
+        
+
+    });
+}
+
+const navLinksChildren = document.querySelectorAll(".nav__link");
+console.log(navLinksChildren)
+
+
+navLinksChildren.forEach((navLink) => {
+    navLink.addEventListener("click", () => {
+        document.querySelector(".nav__link.active")?.classList.remove("active");
+        navLink.classList.add("active");
+        
+    });
+});
+
+document.addEventListener("click", (event) => {
+
+    if(!hamburger_icon.contains(event.target) && !document.querySelector(".hamburger-menu").contains(event.target)) {
+        
+        document.querySelector(".hamburger-menu").classList.remove("active");
+
+    }
+
+
+});
